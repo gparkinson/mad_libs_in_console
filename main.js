fc0c760c2009db6da83e737d73181746b94a8c52
gparkinson <gregoryparkinson@gmail.com>
2015-06-02 20:30:34 -0700

    message received, over and out

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-alert("I'm working in JS")
+// alert("I'm working in JS") //
+
+
+var storytext = "Well, we had all these children out planting trees, see, because we figured that ... that was part of their education, to see how, you know, the root systems ... and also the sense of responsibility, taking care of things, being individually responsible. You know what I mean. And the trees all died. They were orange trees. I don’t know why they died, they just died. Something wrong with the soil possibly or maybe the stuff we got from the nursery wasn’t the best. We complained about it. So we’ve got thirty kids there, each kid had his or her own little tree to plant and we’ve got these thirty dead trees. All these kids looking at these little brown sticks, it was depressing."
+
+function mad_libs(text) {
+	document.write(text)
+}
+
+
+
