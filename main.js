4e23ff01d673df757a0f9d8ded4ddf64745c1ae4
gparkinson <gregoryparkinson@gmail.com>
2015-06-04 17:02:55 -0700

    works for a short story

--- main.js
+++ main.js
@@ -1,33 +1,62 @@
 // alert("I'm working in JS") //
-var storytext = "text text text";
 
-function mad_libs(storytext) {
-	// var storytext = "Well, we had all these " ;
-					// get_noun(2) +
-					// "children out planting trees, see, because we figured that ... that was part of their education, to see how, you know, the root systems ... and also the sense of responsibility, taking care of things, being individually responsible. You know what I mean. And the trees all died. They were orange trees. I don’t know why they died, they just died. Something wrong with the soil possibly or maybe the stuff we got from the nursery wasn’t the best. We complained about it. So we’ve got thirty kids there, each kid had his or her own little tree to plant and we’ve got these thirty dead trees. All these kids looking at these little brown sticks, it was depressing."
 
-	document.write(storytext);
+function mad_libs() {
+ var storytext = "There were 15 " +
+					get_noun(2) +
+					" " +
+					get_gerund() +
+				   "." +
+				   " The " +
+				   get_noun(2) + 
+				   " saw the " +
+				   get_noun(1) +
+				   " " +
+				   get_verb() +
+				   "."
+
+document.write(storytext);
 }
 
-console.log("is this being run" storytext)
 
-// mad_libs();
+var verbs = ["eat","sleep","talk","agitate","churn","cry"];
+var nouns = [["children",1], ["boy",0], ["tree",0], ["stick",0], ["soil",1], ["sheep",1]]	;
 
-function noun_agreement(word,count)
-{if (count==1) 
-	{return word;}
-	else
-		{if (word == "children") 
-			{return word;}
-			else
-			{return word+ "s";}}
-
-}
+function noun_agreement(wordpair) {
+	console.log("wordpair" + wordpair)
+	var flag = wordpair[1];
+	var word = wordpair[0];
+	console.log(flag);
+	console.log(word);
+	{if (flag == 1)
+		{return word;}
+		else
+		{return word+ "s";}}
+	}
 
 function get_noun(count) {
-	var nouns = ["children", "boy", "tree", "stick", "soil"];
 	var nouncount = nouns.length;
-	var nounnumber = Math.floor((Math.random() * nouncount) + 1);
+	var nounnumber = Math.floor((Math.random() * nouncount));
+	console.log("nounnumber " + nounnumber);
 	var noun = nouns[nounnumber];
-	return noun_agreement(noun,count);
+	console.log("noun: "+ noun);
+	return noun_agreement(noun);
 }
+
+function get_verb() {
+    var verbcount = verbs.length;
+	var verbnumber = Math.floor((Math.random() * verbcount));
+	var verb = verbs[verbnumber];
+	console.log("verb: "+ verb);
+	return verb;
+}
+
+function get_gerund() {
+    var verbcount = verbs.length;
+    console.log("verbs " + verbs);
+	var verbnumber = Math.floor((Math.random() * verbcount));
+	var verb = verbs[verbnumber];
+	console.log("gerund: "+ verb);
+	return verb + "ing";
+}
+
