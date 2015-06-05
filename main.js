87a0c084433abb78bdac4978efb7c7196785d7a6
gparkinson <gregoryparkinson@gmail.com>
2015-06-04 18:56:20 -0700

    fix noun count

--- main.js
+++ main.js
@@ -22,41 +22,34 @@ document.write(storytext);
 var verbs = ["eat","sleep","talk","agitate","churn","cry"];
 var nouns = [["children",1], ["boy",0], ["tree",0], ["stick",0], ["soil",1], ["sheep",1]]	;
 
-function noun_agreement(wordpair) {
-	console.log("wordpair" + wordpair)
-	var flag = wordpair[1];
-	var word = wordpair[0];
-	console.log(flag);
-	console.log(word);
-	{if (flag == 1)
-		{return word;}
-		else
-		{return word+ "s";}}
-	}
 
 function get_noun(count) {
 	var nouncount = nouns.length;
 	var nounnumber = Math.floor((Math.random() * nouncount));
-	console.log("nounnumber " + nounnumber);
 	var noun = nouns[nounnumber];
-	console.log("noun: "+ noun);
-	return noun_agreement(noun);
+	return noun_agreement(noun,count);
 }
 
 function get_verb() {
     var verbcount = verbs.length;
 	var verbnumber = Math.floor((Math.random() * verbcount));
 	var verb = verbs[verbnumber];
-	console.log("verb: "+ verb);
 	return verb;
 }
 
 function get_gerund() {
     var verbcount = verbs.length;
-    console.log("verbs " + verbs);
 	var verbnumber = Math.floor((Math.random() * verbcount));
 	var verb = verbs[verbnumber];
-	console.log("gerund: "+ verb);
 	return verb + "ing";
 }
 
+function noun_agreement(wordpair,count) {
+	var flag = wordpair[1];
+	var word = wordpair[0];
+	{if (count == 1) return word}
+	{if (flag == 1)
+		{return word;}
+		else
+		{return word+ "s";}}
+	}
